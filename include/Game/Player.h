e365eaca4007217a4d3946a1ec2c9a20f09d2181
Aquillot <83299370+Aquillot@users.noreply.github.com>
2024-02-12 22:34:48 +0100

    feat (player): Implementation of Attributes and Methods for the Player
    - Added a new attribute for movement to the Player object, including:
        - 'RIGHT' and 'LEFT' constants for direction.
        - 'LIMIT_TIME_JUMP' constant for the time limit of the jump.
        - 'speedMax', the maximum speed of the player.
        - 'timeJump' Time elapsed since the player started jumping
        - Three boolean variables for "on the platform," "is jumping," and "want to jump."
        - Two boolean for horizontal movement
    - Added new methods to the Player object:
        - 'getVerticesHorizontal': Returns a vector of Points representing vertices, with added margin to capture walls.
        - 'getVerticesGround': Returns same as above but to capture the ground.
        - 'getVerticesRoof': Returns same as above but to capture the roof.

--- include/Game/Player.h
+++ include/Game/Player.h
@@ -16,27 +16,61 @@
 
 class Player {
 public:
+    const int RIGHT = 1;/**< Constant to the direction of the player here right*/
+    const int LEFT = -1;/**< Constant to the direction of the player here left*/
+
+    const float LIMIT_TIME_JUMP = 6;
+
     int x; /**< The x-coordinate of the player's position. (in pixels) */
     int y; /**< The y-coordinate of the player's position. */
     int speed; /**< The speed of the player. */
+    int speedMax;/**< The maximum speed of the player. */
     int width; /**< The width of the player. (in pixels) */
     int height; /**< The height of the player. */
+
+    bool canMove = true; /**< If the player can move */
+    bool wantMove = false; /**< If the player try to move */
+
+    float timeJump=0.; /**<The time that has elapsed since the player started jumping */
+    bool isOnPlatform = false; /**< If the player is on a platform */
+    bool isJumping = false;/**< If the player is in a jump */
+    bool wantToJump = false;/**< If the player try to jump */
 
     /**
      * @brief Constructor for the Player class.
      * @param startX Initial x-coordinate of the player.
      * @param startY Initial y-coordinate of the player.
      * @param playerSpeed Speed of the player.
+     * @param speedMax Maximum player speed.
      * @param playerWidth Width of the player.
      * @param playerHeight Height of the player.
      */
-    Player(int startX, int startY, int playerSpeed, int playerWidth, int playerHeight);
+    Player(int startX, int startY, int playerSpeed, int speedMax, int playerWidth, int playerHeight);
 
     /**
      * @brief Get the vertices of the player's bounding box.
      * @return A vector of Point representing the vertices.
      */
     [[nodiscard]] std::vector<Point> getVertices() const;
+
+    /**
+     * @brief Gets the vertices of the player's bounding box, adjusted to capture the wall.
+     * @param direction The direction in which the player intends to move.
+     * @return A vector of Point representing vertices, with added margin to capture wall within the area.
+     */
+    [[nodiscard]] std::vector<Point> getVerticesHorizontal(int direction) const;
+
+    /**
+     * @brief Gets the vertices of the player's bounding box, adjusted to capture the ground.
+     * @return A vector of Point representing vertices, with added margin to capture ground within the area.
+     */
+    [[nodiscard]] std::vector<Point> getVerticesGround() const;
+
+    /**
+     * @brief Gets the vertices of the player's bounding box, adjusted to capture the roof.
+     * @return A vector of Point representing vertices, with added margin to capture roof within the area.
+     */
+    [[nodiscard]] std::vector<Point> getVerticesRoof() const;
 };
 
 #endif //PLAY_TOGETHER_PLAYER_H
--- src/Game/Player.cpp
+++ src/Game/Player.cpp
@@ -5,8 +5,8 @@
  * @brief Implements the Player class representing a player in a 2D game.
  */
 
-Player::Player(int startX, int startY, int playerSpeed, int playerWidth, int playerHeight)
-        : x(startX), y(startY), speed(playerSpeed), width(playerWidth), height(playerHeight) {}
+Player::Player(int startX, int startY, int playerSpeed,int speedMax, int playerWidth, int playerHeight)
+        : x(startX), y(startY), speed(playerSpeed), speedMax(speedMax), width(playerWidth), height(playerHeight){}
 
 std::vector<Point> Player::getVertices() const {
     // Return the vertices of the player's bounding box.
@@ -15,5 +15,33 @@ std::vector<Point> Player::getVertices() const {
             {x + width, y},
             {x + width, y + height},
             {x, y + height}
+    };
+}
+
+std::vector<Point> Player::getVerticesHorizontal(int direction) const {
+    // Return the vertices of the player's bounding box, with added margin to capture wall within the area.
+    return {
+            {x + (direction * 2),         y +2},
+            {x + width + (direction * 2), y +2},
+            {x + width +(direction * 2), y + height - 2},
+            {x + (direction * 2),         y + height - 2}
+    };
+}
+std::vector<Point> Player::getVerticesGround() const {
+    // Return the vertices of the player's bounding box, with added margin to capture ground within the area.
+    return {
+            {x + 2,         y + height},
+            {x + width - 2, y + height},
+            {x + width - 2, y + height + 2},
+            {x + 2,         y + height + 2}
+    };
+}
+std::vector<Point> Player::getVerticesRoof() const {
+    // Return the vertices of the player's bounding box, with added margin to capture roof within the area.
+    return {
+            {x + 2,         y + 2},
+            {x + width - 2, y + 2},
+            {x + width - 2,  y },
+            {x + 2,         y }
     };
 }
