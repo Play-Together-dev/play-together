bc909ed08978f2bde8a98605459bdb2dce1a67b1
Aquillot <83299370+Aquillot@users.noreply.github.com>
2024-02-12 23:17:02 +0100

    refactor (main) initialPlayer must initialize speedMax

--- src/Main.cpp
+++ src/Main.cpp
@@ -31,7 +31,7 @@ int main([[maybe_unused]] int argc, [[maybe_unused]] char *args[]) {
 
     SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
-    Player initialPlayer(50, 50, 2, 20, 30);
+    Player initialPlayer(50, 50, 1,2, 20, 30);
     Game game(window, renderer, initialPlayer);
     game.loadPolygonsFromMap("experimentation");
 
