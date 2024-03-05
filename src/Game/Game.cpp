adb5d8a7b103347380b780c9b2a6b7805830b0cc
Jhune <donovann.dev@gmail.com>
2024-03-05 21:06:58 +0100

    Revert "Saves"

--- src/Game/Game.cpp
+++ src/Game/Game.cpp
@@ -69,13 +69,6 @@ bool checkAABBCollision(const SDL_FRect &a, const SDL_FRect &b) {
 
 /** METHODS **/
 
-Game ::Game(){
-        this->window = SDL_CreateWindow("Play Together", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT,
-                                        SDL_WINDOW_SHOWN);
-        this->renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-        this->player = Player (-50, 50, 2, 20, 30);
-}
-
 void Game::addCharacter(const Player &character) {
     characters.push_back(character);
 }
